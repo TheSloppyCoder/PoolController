396501207aa671fd42dc8f7ebc8d05fb5f10f826
FanieDuvenage <duvenage911@gmail.com>
2025-06-25 10:32:33 +0200

    Initial Commit.

---
+++ src/InfoWindow.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace PoolController;
+
+public partial class InfoWindow : Window
+{
+    public InfoWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnInfoExit_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
